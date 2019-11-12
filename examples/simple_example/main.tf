8965102bb929f39217bff2ee3ff92d6804c4bda2
Josh Hill <josh.hill@cloudsolutions.co.uk>
2019-11-12 12:34:37 +0000

    Adds lookups to steps
    This ensures that users don't need to define everything in a step even
    when things are empty or not required.

--- examples/simple_example/main.tf
+++ examples/simple_example/main.tf
@@ -15,7 +15,7 @@
  */
 
 terraform {
-  required_version = "0.12.3"
+  required_version = "0.12.13"
 }
 
 provider "google" {
@@ -23,39 +23,39 @@ provider "google" {
 }
 
 module "cicd" {
-    source = "../../"
-    
-    trigger_description = "Builds the kms-secrets-operator image."
-    repo_name = "kms-secrets-operator"
-    project = var.project
-    ignored_files = [
-        ".gitignore",
-    ]
-    branch_name = "master"
-    images = [
-        "gcr.io/$PROJECT_ID/kms-secrets-operator:$COMMIT_SHA"
-    ]
-    build_tags = [
-        "kms-secrets-operator",
-        "image",
-        "build"
-    ]
-    steps = [
-        {
-            name = "gcr.io/cloud-builders/docker"
-            args = [
-                "build",
-                "-t",
-                "gcr.io/$PROJECT_ID/kms-secrets-operator:$COMMIT_SHA",
-                "."
-            ]
-            env = []
-            id = ""
-            entrypoint = ""
-            dir = ""
-            timeout = ""
-            volumes = []
-            wait_for = []
-        }
-    ]
+  source = "../../"
+
+  trigger_description = "Builds the kms-secrets-operator image."
+  repo_name           = "kms-secrets-operator"
+  project             = var.project
+  ignored_files = [
+    ".gitignore",
+  ]
+  branch_name = "master"
+  images = [
+    "gcr.io/$PROJECT_ID/kms-secrets-operator:$COMMIT_SHA"
+  ]
+  build_tags = [
+    "kms-secrets-operator",
+    "image",
+    "build"
+  ]
+  steps = [
+    {
+      name = "gcr.io/cloud-builders/docker"
+      args = [
+        "build",
+        "-t",
+        "gcr.io/$PROJECT_ID/kms-secrets-operator:$COMMIT_SHA",
+        "."
+      ]
+      env        = []
+      id         = ""
+      entrypoint = ""
+      dir        = ""
+      timeout    = ""
+      volumes    = []
+      wait_for   = []
+    }
+  ]
 }

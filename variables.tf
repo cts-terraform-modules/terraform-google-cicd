85f801440eafc2a2f6981853ec3913fffa637d4e
Josh Hill <josh.hill@cloudsolutions.co.uk>
2020-02-03 14:26:08 +0000

    Update repo dir default

--- variables.tf
+++ variables.tf
@@ -28,7 +28,7 @@ variable "create_repo" {
 variable "repo_dir" {
   description = "(Optional) If you want to push code to the created repo, this is the local dir where the code is stored."
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "project" {
